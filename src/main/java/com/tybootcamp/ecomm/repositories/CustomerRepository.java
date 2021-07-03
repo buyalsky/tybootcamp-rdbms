d84f91bf3893a8f09ef38c3fbed2fd899f6cf091
Burak <burak.dursunlar@hotmail.com>
2021-07-03 19:12:49 +0300

    Cleanup

--- src/main/java/com/tybootcamp/ecomm/repositories/CustomerRepository.java
+++ src/main/java/com/tybootcamp/ecomm/repositories/CustomerRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
-    Customer findByName(String name);// select * from customer where name=?
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
